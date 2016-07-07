898c67d98e9cb662563d5082d0e283ca0572b715
Francesco Carmagnola <fcarmagnola@tuitravel-ad.net>
2016-07-07 11:02:27 +0200

    First Commit

---
+++ app.js
@@ -0,0 +1,77 @@
+var fs = require('fs');
+var xml2js = require('xml2js');
+var json2csv = require('json2csv');
+var fieldSchema = ['fullName','label','description','type','length','required','unique',
+              'trackHistory','trackTrending','trackFeedHistory','formula','pickListValues', 'defaultPicklistValue', 'pickListControllingField'];
+
+
+var objectDirectory = __dirname + '/objects/';
+files = fs.readdirSync(__dirname + '/objects/');
+
+for (var i = 0; i < files.length; i++) {
+  analyzeObjectFile(files[i]);
+}
+
+
+
+function analyzeObjectFile(objectName) {
+  var parser = new xml2js.Parser();
+  fs.readFile(objectDirectory + objectName, function(err, data) {
+      parser.parseString(data, function (err, result) {
+          var objData = result.CustomObject.fields;
+          for (var i in objData) {
+            if (objData.hasOwnProperty(i)) {
+              var element = objData[i];
+              arrayToString(element);
+            }
+          }
+          json2csv({ data: objData, fields: fieldSchema}, function(err, csv) {
+            fs.writeFile('results/'+objectName.replace('.object','') + '.csv', csv, function(err) {
+              if (err){
+                throw err;
+              }
+              console.log(objectName + ': file saved');
+            });
+          });
+
+      });
+  });
+}
+
+function arrayToString(element) {
+  for (var variable in element) {
+    if (element.hasOwnProperty(variable)) {
+      if(variable == 'picklist'){
+        formatPickListValues(element,'picklist');
+      }
+
+      if(typeof element[variable][0] == 'string'){
+        element[variable] = element[variable][0];
+      } else {
+        arrayToString(element[variable]);
+      }
+    }
+  }
+}
+
+function formatPickListValues(element,propertyName) {
+  var controllingField = '';
+  if(element[propertyName][0].hasOwnProperty('controllingField')){
+    controllingField = element[propertyName][0].controllingField[0];
+  }
+  var pickListValues = element[propertyName][0].picklistValues;
+  var pickListValuesToString = '';
+  var defaultPicklistValue = '';
+  for (var i in pickListValues) {
+    if (pickListValues.hasOwnProperty(i)) {
+      pickListValuesToString += pickListValues[i].fullName[0] + ' - ';
+      if(pickListValues[i].default[0]){
+        defaultPicklistValue = pickListValues[i].fullName[0];
+      }
+    }
+  }
+  pickListValuesToString = pickListValuesToString.slice(0, -3);
+  element.pickListValues = pickListValuesToString;
+  element.defaultPicklistValue = defaultPicklistValue;
+  element.pickListControllingField = controllingField;
+}
