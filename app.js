7377cd56e91baa68b9d7d87327f760783242fe21
fracarma <fracarma@libero.it>
2017-01-31 20:35:58 +0100

    Create also a resume result with only the lookup objects

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ var fieldSchema = ['fullName','label','description','type','referenceTo','length
 var objectDirectory = __dirname + '/objects/';
 files = fs.readdirSync(__dirname + '/objects/');
 var writeStream = fs.createWriteStream('resultUML.txt');
+var partialWriteStream = fs.createWriteStream('resumeResultUML.txt');
 var writeStreamObjects = fs.createWriteStream('objectDeclaration.txt');
+var partialwriteStreamObjects = fs.createWriteStream('partialObjectDeclaration.txt');
 
 for (var i = 0; i < files.length; i++) {
   analyzeObjectFile(files[i]);
@@ -22,19 +24,35 @@ function analyzeObjectFile(objectName) {
       parser.parseString(data, function (err, result) {
           var objData = result.CustomObject.fields;
           var label = (result.CustomObject.label) ? result.CustomObject.label : objectName.replace('.object','');
-          writeStreamObjects.write('object ' +'\"' +label+'\"' +' as '+objectName.replace('.object','') +'\n');
+          var objDeclaration = 'object ' +'\"' +label+'\"' +' as '+objectName.replace('.object','');
+          writeStreamObjects.write(objDeclaration+'\n');
+
+          var referenceSet = [];
+          var hasLookUp = false;
           for (var i in objData) {
             if (objData.hasOwnProperty(i)) {
               var element = objData[i];
-              if(element.type[0] === 'Lookup'){
-                var reference = (element.referenceTo) ? element.referenceTo : (''+element.fullName).replace('Id','');
-                reference = (reference === 'Parent') ? label : reference;
-                var rowToWrite = objectName.replace('.object','') + ' <|-- '+ '\"'+element.fullName+'\" ' + reference+'\n';
-                console.log(rowToWrite);
-
-                writeStream.write(rowToWrite);
+              if(element.type[0] !== 'Lookup'){
+                continue;
               }
+              hasLookUp = true;
+              var reference = (element.referenceTo) ? element.referenceTo : (''+element.fullName).replace('Id','');
+              reference = (reference === 'Parent') ? label : reference;
+              var baseRow = objectName.replace('.object','') + ' --|> ' + reference;
+              var simplerRow = baseRow+'\n';
+              var rowToWrite = baseRow+' : '+element.fullName+'\n';
+              writeStream.write(rowToWrite);
+              if(referenceSet.indexOf(reference[0]) > -1){
+                continue;
+              }
+              referenceSet.push(reference[0]);
+              partialWriteStream.write(simplerRow);
+
             }
+          }
+
+          if(hasLookUp){
+            partialwriteStreamObjects.write(objDeclaration+'\n');
           }
       });
   });
