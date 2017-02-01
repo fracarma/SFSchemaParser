f6a9ac9b123b04dbe8e900d7083c7e874d0b8f86
fracarma@libero.it <fracarma@libero.it>
2017-02-01 17:25:01 +0100

    Let the single UML fiels work!
    Remember to create the folders..
    
    results/
    results/singleObjects/
    results/svgUML/singleObjectsSVG

--- generateSVG.js
+++ generateSVG.js
@@ -1,5 +1,23 @@
 var plantuml = require('node-plantuml');
 var fs = require('fs');
 
-var gen = plantuml.generate("completeResultUML.txt",{format: 'svg'});
-gen.out.pipe(fs.createWriteStream("output-file.svg"));
+var resultsDirectory = __dirname + '/results/';
+var resultsSingleObjectDirectory = resultsDirectory + 'singleObjects/';
+var svgUMLDirectory = resultsDirectory + 'svgUML/';
+var svgUMLSingleObjectsDirectory = svgUMLDirectory + 'singleObjectsSVG/';
+
+// var gen = plantuml.generate(resultsDirectory+'/completeResultUML.txt',{format: 'svg'});
+// gen.out.pipe(fs.createWriteStream(svgUMLDirectory+"completeResultUML.svg"));
+
+files = fs.readdirSync(resultsSingleObjectDirectory);
+
+for (var i = 0; i < files.length; i++) {
+  generateUML(files[i]);
+}
+
+function generateUML(objectName){
+  var fileLocation = 'results/singleObjects/'+objectName;
+  var gen = plantuml.generate(fileLocation,{format: 'svg'});
+  gen.out.pipe(fs.createWriteStream(svgUMLSingleObjectsDirectory+objectName.replace('uml','')+"svg"));
+  console.log(fileLocation);
+}
